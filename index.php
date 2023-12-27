3b198ec01e32efc2ac44f03bee05ae5847e73f99
zaldyprtm <zaldyprtm@gmail.com>
2023-12-27 21:38:47 +0700

    php

--- index.php
+++ index.php
@@ -9,6 +9,29 @@
     <title>Sign Up Instagram</title>
 </head>
 <body>
+
+<?php
+    require('./connection.php');
+    if (isset($_POST['signUP_button'])) {
+        $email=$_POST['email'];
+        $fullname=$_POST['fullname'];
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+
+            $p=crud::conect()->prepare('INSERT INTO crudtable(email,fullname,username,pass) VALUES(:e,:f,:u,:p)');
+            $p->bindValue(':e',$email);
+            $p->bindValue(':f',$fullname);
+            $p->bindValue(':u',$username);
+            $p->bindValue(':p',$password);
+            $p->execute();
+            echo 'berhasil';
+            header('location:login.php');
+        }
+
+
+?>
+
+
 <div class="instagram-container">
     <div class="instagram-logo">
         <img src="assets/instagram-logo.png" alt="logo">
@@ -23,12 +46,13 @@
     <div class="instagram-container-inside">
         <button><i class="fa-brands fa-facebook fa-bounce fa-xl"></i>&nbsp; Login With Facebook</button>
         <h5><hr>OR</h5>
-        <input type="email" placeholder="Mobile Number or Email">
-        <input type="text" placeholder="Full Name">
-        <input type="text" placeholder="Username">
-        <input type="password" placeholder="Password">
-        <button>Sign Up</button>
-
+        <form action="" method="post">
+        <input type="email" name="email" placeholder="Mobile Number or Email">
+        <input type="text" name="fullnme" placeholder="Full Name">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Sign Up" name="signUP_button">
+</form>
         <p>People who use our service may have uploaded </p>
         <p>your contact information to Instagram. Learn More</p>
         <p>By signing up, you agree to our Terms , Privacy </p>
@@ -36,7 +60,7 @@
     </div>
     </div>
     <div class="instagram-bottom-container">
-        <h4>Have an account? <a href="login.html" style="text-decoration: none; color: #3897f0;">Login</a></h4>
+        <h4>Have an account? <a href="login.php" style="text-decoration: none; color: #3897f0;">Login</a></h4>
     </div>
 </div>
 
--- login.php
+++ login.php
@@ -9,23 +9,55 @@
     <title>Login Instagram</title>
 </head>
 <body>
+
+<?php
+    require('./connection.php');
+
+    if (isset($_POST['login_button'])) {
+        $_SESSION['validate']=false;
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        $p=crud::conect()->prepare('SELECT * FROM crudtable WHERE username=:u and pass=:p');  
+        $p->bindValue(':u, $username');
+        $p->bindValue(':p, $password');
+        $p->execute();
+        $d=$p->fetchAll(PDO::FETCH_ASSOC);
+        if ($p->rowCount()>0) {
+            $_SESSION['username']=$username;
+            $_SESSION['pass']=$password;
+            $_SESSION['validate']=true;
+            header('location:index.php');
+            echo 'Login berhasil';
+        }else{
+            echo 'make sure that you are registered';
+        }
+    }
+
+
+
+
+?>
+
+
     <div class="instagram-container">
         <div class="instagram-logo">
             <img src="assets/instagram-logo.png" alt="logo">
         </div>
 
         <div class="instagram-container-inside">
-            <input type="text" placeholder="Phone number, Username, or email">
-            <input type="password" placeholder="Password">
-            <button>Log In</button>
+            <form action="" method="post">
+            <input type="text" name="username" placeholder="Phone number, Username, or email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Login" name="login_button">
             <h5><hr>OR</h5>
             <button><i class="fa-brands fa-facebook fa-fade fa-xl"></i>&nbsp; Login With Facebook</button>
             <a href="#">Forgot password?</a>
+            </form>
         </div>
         </div>
 
         <div class="instagram-bottom-container">
-            <h4>Don't have an account? &nbsp;<a href="index.html" style="text-decoration: none; color: #3897f0;">Sign Up</a></h4>
+            <h4>Don't have an account? &nbsp;<a href="index.php" style="text-decoration: none; color: #3897f0;">Sign Up</a></h4>
         </div>
         <div class="instagram-download">
             <a href="https://play.google.com/store/apps/details?id=com.instagram.android&hl=en_US"><img src="assets/googleplay-button.png" alt="google play store" id="gplay" >
